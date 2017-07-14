ecd741b92e2290194e8f0d846130ab2018397274
Natalie Despain <nataliedespainn@gmail.com>
2017-07-14 14:46:00 -0700

    finished stretch 2

--- playlist.js
+++ playlist.js
@@ -11,17 +11,41 @@ $(document).ready(function() {
     }
 
 
-    // ADD TEXT ON CLICK
+    // // ADD TEXT ON CLICK
+    // $('.scrollImg').click(function(e){
+    //   var id = Number(e.target.id);
+    //
+    //   for (var item of result.results) {
+    //     if (item.id === id) {
+    //       var text = $(`<h6>${item.artist}: ${item.title}</h6>`);
+    //       $('.box').append(text);
+    //     }
+    //   }
+    //
+    // })
+
+    // CHANGE CURRENT SELECTION
     $('.scrollImg').click(function(e){
+      $('.album-tracks').empty();
       var id = Number(e.target.id);
 
       for (var item of result.results) {
         if (item.id === id) {
-          var text = $(`<h6>${item.artist}: ${item.title}</h6>`);
-          $('.box').append(text);
+          $('#current-album').text(`${item.artist}: ${item.title}`);
+          $('.current-album-image').css({'background': `url('images/${item.cover_art}')`, 'background-size': 'cover'})
+          for (var i = 0; i < item.tracks.length; i++) {
+            var track = $(`<h6>${item.tracks[i].title} - ${item.tracks[i].length}</span></h6>`);
+            $('.album-tracks').append(track);
+          }
         }
       }
 
+    })
+
+    // ADD SONG TO PLAYLIST
+    $('.album-tracks').click(function(e){
+      var song = $(`<h6>${e.target.innerText}</span></h6>`);
+      $('.playlist-tracks').append(song);
     })
 
   }});
@@ -30,7 +54,7 @@ $(document).ready(function() {
 
   // CLEAR PLAYLIST
   $('#clear').click(function() {
-    $('.box').empty();
+    $('.playlist-tracks').empty();
   })
 
 
@@ -38,9 +62,9 @@ $(document).ready(function() {
   $('#submit').click(function() {
 
     var collection = {};
-    var htmlCollection = $('.box').children();
+    var htmlCollection = $('.playlist-tracks').children();
     for (var i = 0; i < htmlCollection.length; i++) {
-      var split = htmlCollection[i].innerHTML.split(": ");
+      var split = htmlCollection[i].innerHTML.split(" - ");
       collection[split[0]] = split[1];
     }
 
@@ -57,6 +81,31 @@ $(document).ready(function() {
     });
 
   })
+
+
+
+  // $('#submit').click(function() {
+  //
+  //   var collection = {};
+  //   var htmlCollection = $('.box').children();
+  //   for (var i = 0; i < htmlCollection.length; i++) {
+  //     var split = htmlCollection[i].innerHTML.split(": ");
+  //     collection[split[0]] = split[1];
+  //   }
+  //
+  //   var jsonData = JSON.stringify(collection)
+  //   console.log(jsonData)
+  //
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "https://lit-fortress-6467.herokuapp.com/post",
+  //     data: jsonData,
+  //     success: function(msg) {
+  //       console.log(msg);
+  //     }
+  //   });
+  //
+  // })
 
 
 
